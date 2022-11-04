5059ea34596a85accde8eff55f77d0cdd2b2aabf
unknown <msegantine8@gmail.com>
2022-11-04 14:59:36 -0300

    UI calculator finished

---
+++ src/Calculator.tsx
@@ -0,0 +1,45 @@
+import "./styles.css";
+
+function Aspanspan() {
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="content-results">
+          <p className="results">
+            1 + 1
+          </p>
+        </div>
+        
+        <div className="content-numbers">
+          <span className="op">C</span>
+          <span className="op">AP</span>
+          <span className="op">%</span>
+          <span className="op">/</span>
+          <span className="number">7</span>
+          <span className="number">8</span>
+          <span className="number">9</span>
+          <span className="op">x</span>
+          <span className="number">4</span>
+          <span className="number">5</span>
+          <span className="number">6</span>
+          <span className="op">-</span>
+          <span className="number">1</span>
+          <span className="number">2</span>
+          <span className="number">3</span>
+          <span className="op">+</span>
+          <span className="op">
+            <span>+</span>/<span>-</span>
+          </span>
+          <span className="number">0</span>
+          <span className="op">,</span>
+
+          <span className="op equal-signal">=</span>
+
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Aspanspan;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Calculator from "./Calculator";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>
-)
+);
