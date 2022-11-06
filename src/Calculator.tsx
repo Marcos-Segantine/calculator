3d0aec39c5b21ff2f2f4a1f72cf7b5bb56384484
unknown <msegantine8@gmail.com>
2022-11-06 18:03:39 -0300

    Add functions to calculator

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -4,71 +4,111 @@ import deleteIcon from "./assets/tag-x-solid-24.png";
 
 function Calculator() {
   const [valueOutput, setValueOutput] = useState("");
-  const [number, setNumber] = useState("");
+  const [valueOutputPrevius, setOutputPrevius] = useState("");
   const [operation, setOperation] = useState<string[]>([""]);
 
-  const handleNumbersValue = (e: any) => {
+  const handleNumber = (e: any) => {
     setValueOutput(valueOutput + e.target.id);
   };
 
-  const handleOperation = (e: any) => {
-    operation[0] === ""
-      ? (operation.splice(0, 1, valueOutput),
-        operation.splice(1, 1, e.target.id))
-      : "";
+  const handleOperator = (e: any) => {
+    if (!operation[0]) {
+      operation.splice(0, 1, valueOutput);
+      operation.push(e.target.id);
+      setOutputPrevius(operation[0] + " " + operation[1]);
+    } else if (operation[0]) {
+      let result: number;
 
-    setNumber(valueOutput + " " + e.target.id);
+      operation.splice(2, 1, valueOutput);
+      switch (operation[1]) {
+        case "+":
+          result = Number(operation[0]) + Number(operation[2]);
+          setValueOutput(String(result));
+          operation.splice(0, 1, String(result));
+          operation.splice(1, 2);
+
+          break;
+        case "-":
+          result = Number(operation[0]) - Number(operation[2]);
+          setValueOutput(String(result));
+          operation.splice(0, 1, String(result));
+          operation.splice(1, 2);
+          break;
+        case "*":
+          result = Number(operation[0]) * Number(operation[2]);
+          setValueOutput(String(result));
+          operation.splice(0, 1, String(result));
+          operation.splice(1, 2);
+          break;
+        case "/":
+          result = Number(operation[0]) / Number(operation[2]);
+          setValueOutput(String(result));
+          operation.splice(0, 1, String(result));
+          operation.splice(1, 2);
+          break;
+      }
+
+      operation.splice(1, 1, e.target.id);
+      operation.splice(2, 1);
+      setOutputPrevius(operation[0] + " " + operation[1]);
+    }
+
     setValueOutput("");
   };
 
-  const clear = () => {
-    setValueOutput("");
-    setNumber("");
-    setOperation([""]);
-  };
+  const result = () => {
+    if (operation[0] && operation[1]) {
+      operation.push(valueOutput);
+    }
 
-  const getResult = () => {
-    let result = 0;
+    setOutputPrevius("");
 
-    operation[2] ? "" : (
-      operation.push(valueOutput),
-      setValueOutput(number + " " + valueOutput)
-    );
+    let result: number;
 
     switch (operation[1]) {
       case "+":
         result = Number(operation[0]) + Number(operation[2]);
         setValueOutput(String(result));
+        operation.splice(0, 1, String(result));
+        operation.splice(1, 2);
         break;
       case "-":
         result = Number(operation[0]) - Number(operation[2]);
         setValueOutput(String(result));
-        break;
-      case "/":
-        result = Number(operation[0]) / Number(operation[2]);
-        setValueOutput(String(result));
+        operation.splice(0, 1, String(result));
+        operation.splice(1, 2);
         break;
       case "*":
         result = Number(operation[0]) * Number(operation[2]);
         setValueOutput(String(result));
+        operation.splice(0, 1, String(result));
+        operation.splice(1, 2);
         break;
-
-      default:
+      case "/":
+        result = Number(operation[0]) / Number(operation[2]);
+        setValueOutput(String(result));
+        operation.splice(0, 1, String(result));
+        operation.splice(1, 2);
         break;
     }
   };
 
   const erase = () => {
-    let arrValueOutput: any = valueOutput.split("");
-    arrValueOutput.pop();
+    const valueTemp = valueOutput.split('')
+    valueTemp.pop()
+    setValueOutput(valueTemp.join(''))
+  }
 
-    setValueOutput(arrValueOutput.join(""));
+  const clear = () => {
+    setValueOutput("");
+    setOutputPrevius("");
+    setOperation([""]);
   };
 
   return (
     <div className="App">
       <div className="calculator">
-        <span className="previus-value">{number}</span>
+        <span className="previus-value">{valueOutputPrevius}</span>
         <div className="content-value">
           <p className="value">{valueOutput}</p>
         </div>
@@ -81,51 +121,53 @@ function Calculator() {
             <img src={deleteIcon} alt="" />
           </span>
           <span className="op">%</span>
-          <span className="op" id="/" onClick={handleOperation}>
+          <span className="op" id="/" onClick={handleOperator}>
             /
           </span>
-          <span className="number" id="7" onClick={handleNumbersValue}>
+          <span className="number" id="7" onClick={handleNumber}>
             7
           </span>
-          <span className="number" id="8" onClick={handleNumbersValue}>
+          <span className="number" id="8" onClick={handleNumber}>
             8
           </span>
-          <span className="number" id="9" onClick={handleNumbersValue}>
+          <span className="number" id="9" onClick={handleNumber}>
             9
           </span>
-          <span className="op" id="*" onClick={handleOperation}>
+          <span className="op" id="*" onClick={handleOperator}>
             x
           </span>
-          <span className="number" id="4" onClick={handleNumbersValue}>
+          <span className="number" id="4" onClick={handleNumber}>
             4
           </span>
-          <span className="number" id="5" onClick={handleNumbersValue}>
+          <span className="number" id="5" onClick={handleNumber}>
             5
           </span>
-          <span className="number" id="6" onClick={handleNumbersValue}>
+          <span className="number" id="6" onClick={handleNumber}>
             6
           </span>
-          <span className="op" id="-" onClick={handleOperation}>
+          <span className="op" id="-" onClick={handleOperator}>
             -
           </span>
-          <span className="number" id="1" onClick={handleNumbersValue}>
+          <span className="number" id="1" onClick={handleNumber}>
             1
           </span>
-          <span className="number" id="2" onClick={handleNumbersValue}>
+          <span className="number" id="2" onClick={handleNumber}>
             2
           </span>
-          <span className="number" id="3" onClick={handleNumbersValue}>
+          <span className="number" id="3" onClick={handleNumber}>
             3
           </span>
-          <span className="op" id="+" onClick={handleOperation}>
+          <span className="op" id="+" onClick={handleOperator}>
             +
           </span>
           <span className="op">
             <span>+</span>/<span>-</span>
           </span>
-          <span className="number">0</span>
+          <span className="number" id="0" onClick={handleNumber}>
+            0
+          </span>
           <span className="op">,</span>
-          <span className="op equal-signal" onClick={getResult}>
+          <span className="op equal-signal" onClick={result}>
             =
           </span>
         </div>
